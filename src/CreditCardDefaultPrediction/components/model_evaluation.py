85315827c9b7b5a36674fcdfe9fc69515b3b2301
Shreyash waralkar <waralkarshayu@gmail.com>
2024-01-01 00:41:31 +0530

    file updated

--- src/CreditCardDefaultPrediction/components/model_evaluation.py
+++ src/CreditCardDefaultPrediction/components/model_evaluation.py
@@ -25,10 +25,10 @@ class ModelEvaluation:
             model_path=os.path.join("artifacts","model.pkl")
             model=load_object(model_path)
 
-            mlflow.set_registry_uri("https://dagshub.com/waralkarshayu/credit_card_default_prediction.mlflow")
+            # mlflow.set_registry_uri("https://dagshub.com/waralkarshayu/credit_card_default_prediction.mlflow")
 
-            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-            print(tracking_url_type_store)
+            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            # print(tracking_url_type_store)
 
 
             with mlflow.start_run():
@@ -41,16 +41,16 @@ class ModelEvaluation:
                 
                 # This condition is for the dagshub
                 # Model registry does not work with file store
-                if tracking_url_type_store != "file":
+                # if tracking_url_type_store != "file":
 
                     # Register the model
                     # There are other ways to use the Model Registry, which depends on the use case,
                     # please refer to the doc for more information:
                     # https://mlflow.org/docs/latest/model-registry.html#api-workflow 
-                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+                    # mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
                 # It is for local    
-                else:
-                    mlflow.sklearn.log_model(model, "model")
+                # else:
+                    # mlflow.sklearn.log_model(model, "model")
 
         except Exception as e:
             raise e
